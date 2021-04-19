f2f58dc36c60132a6706ace78d9c6918d4ddc9c9
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-04-19 12:10:40 -0300

    backing to the basics

--- .github/actions/action-build-tests/entrypoint.sh
+++ .github/actions/action-build-tests/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# echo "Hello $1"
-# time=$(date)
-# echo "::set-output name=time::$time"
+echo "String received: $1"
+time=$(date)
+echo "::set-output name=time::$time"
 
-echo "Build executable..."
-gcc -o myapp src/main.c
-./myapp
+# echo "Build executable..."
+# gcc -o myapp src/main.c
+# ./myapp
